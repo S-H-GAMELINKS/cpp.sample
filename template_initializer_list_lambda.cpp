13899111a2e4ec567d5061d6ee7504ce24635269
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-07-08 19:28:40 +0900

    add template_initializer_list_lambda.cpp

---
+++ template_initializer_list_lambda.cpp
@@ -0,0 +1,25 @@
+// Template Initializer List Sample
+// Compile to "g++ template_initializer_list_lambda.cpp -std=c++17"
+#include <iostream>
+#include <vector>
+#include <functional>
+
+template<class T>
+class Container {
+public:
+    std::vector<T> container;
+    Container(std::initializer_list<T> init) : container(init.begin(), init.end()) {};
+};
+
+int main(){
+    
+    std::function<void()> Func1 = []() constexpr {std::cout << "Func1" << std::endl;};
+    std::function<void()> Func2 = []() constexpr {std::cout << "Func2" << std::endl;};
+
+    Container<std::function<void()>> Con = {Func1, Func2};
+
+    for (auto&& c : Con.container)
+        c();
+
+    return 0;
+}
