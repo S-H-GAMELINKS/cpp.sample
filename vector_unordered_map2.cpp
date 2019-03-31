7322b5b88d22a9447501cbf7f618ffedfd4e2b66
S.H <gamelinks007@gmail.com>
2019-03-31 18:50:54 +0900

    Create vector_unordered_map2.cpp

---
+++ vector_unordered_map2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+int main() {
+    
+    std::vector<std::unordered_map<std::int32_t, std::string>> talks;
+    
+    std::string str = "hoge";
+    
+    for(int i = 0; i < 10; i++) {
+        try {
+            auto& talk = talks.at(0);
+            std::cout << "DB is exit!" << std::endl;
+            std::cout << "Insert data!" << std::endl;
+            talk.emplace(talk.size(), str);
+        } catch(std::out_of_range& ex) {
+            std::cout << "Out of Range!" << std::endl;
+            std::cout << "Create New DB!" << std::endl;
+        
+            std::unordered_map<std::int32_t, std::string> new_talk;
+            
+            new_talk.emplace(new_talk.size(), str);
+        
+            talks.emplace_back(std::move(new_talk));
+        }
+    }
+    auto t = talks.at(0);
+    
+    for(auto&& u : t)
+        std::cout << "id: " << u.first << ", content: " << u.second << std::endl;
+    
+    return 0;
+}
