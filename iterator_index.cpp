e0707baadecee976e7417652cd97fb7f9f324001
S.H <gamelinks007@gmail.com>
2019-07-29 23:00:37 +0900

    Create iterator_index.cpp

---
+++ iterator_index.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <array>
+
+int main() {
+    
+    std::array<int, 10> data = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    
+    for(decltype(data)::iterator it = std::begin(data); std::end(data) != it; it++)
+        std::cout << *it << std::distance(std::begin(data), it) << std::endl;
+ 
+    return 0;
+}
