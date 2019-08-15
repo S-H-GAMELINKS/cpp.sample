560bdb31bc9d23570f2ed0685c6003f4ade77948
S.H <gamelinks007@gmail.com>
2019-08-15 12:19:35 +0900

    Create template_specilization_inheritance.cpp

---
+++ template_specilization_inheritance.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+#include <typeinfo>
+
+template <class T>
+class Any : public T {};
+
+template <>
+class Any<int> {
+    int value = 0;
+    public:
+    Any() = default;
+    ~Any() = default;
+    Any& operator=(const Any& any) { this->value = any.value; return *this; }
+    Any& operator+(const Any& any) { this->value +=any.value; return *this;}
+    void operator=(const int& var) {this->value = var;}
+    void operator+=(const int& var) {this->value += var;}
+    void operator-=(const int& var) {this->value -= var; }
+    Any(const int& var) {this->value = var;}
+    int show() {return this->value;}
+};
+
+int main() {
+    
+    Any<std::string> any;
+    
+    std::cout << typeid(any).name() << std::endl;
+    
+    any += std::string("hoge");
+    
+    std::cout << any << std::endl;
+    
+    Any<int> hoge(100);
+    
+    std::cout << hoge.show() << std::endl;
+    
+    hoge = 42;
+    
+    std::cout << hoge.show() << std::endl;
+    
+    hoge += 42;
+    
+    std::cout << hoge.show() << std::endl;
+    
+    hoge -= 42;
+    
+    std::cout << hoge.show() << std::endl;
+    
+    Any<int> h(100);
+    
+    h = h + hoge;
+    
+    std::cout << h.show() << std::endl;
+    
+    h = hoge;
+    
+    std::cout << h.show() << std::endl;
+    
+    return 0;
+}
